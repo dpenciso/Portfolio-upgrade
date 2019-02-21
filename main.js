092c08aa7bf53e3c50b42925182c568d7c5582dd
David Enciso <dpenciso@msn.com>
2019-02-21 12:43:32 -0700

    imported more starwars api and put films and crawl in intro

---
+++ main.js
@@ -0,0 +1,22 @@
+import { films } from './films.js'
+import {people} from './people.js'
+
+const intro = document.querySelector('.intro')
+
+films.forEach((film)=> {
+    let titleElement = document.createElement('h1')
+    let crawlElement = document.createElement('h3')
+
+    titleElement.textContent = film.title
+    crawlElement.textContent = film.opening_crawl
+
+    intro.appendChild(titleElement)
+    intro.appendChild(crawlElement)
+})
+
+
+// let titleElement = document.querySelector('.title')
+// let crawlElement = document.querySelector('.crawl')
+
+// titleElement.textContent = films[0].title
+// crawlElement.textContent = films[0].opening_crawl
