d4980a097e72d58c672f09e2aa366aff9f7ee252
David Enciso <dpenciso@msn.com>
2019-02-26 10:51:32 -0700

    Set up basic star wars film tiles

--- main.js
+++ main.js
@@ -1,26 +1,28 @@
-import { films } from './films.js'
-import {people} from './people.js'
+import { films } from "./films.js";
+import { people } from "./people.js";
 
-const intro = document.querySelector('.intro')
+const intro = document.querySelector(".intro");
 
-films.sort ((a,b) => (a.episode_id > b.episode_id) ? 1 : -1 )
+films.sort((a, b) => (a.episode_id > b.episode_id ? 1 : -1));
 
+// const sorted = films.sort ((a,b) => (a.episode_id > b.episode_id) ? 1: -1)
 
+//console.log(sorted)
 
-films.forEach((film)=> {
-    let titleElement = document.createElement('h1')
-    let crawlElement = document.createElement('h3')
-    let lineBreak = document.createElement ('hr')
-    intro.appendChild (lineBreak)
+films.forEach(film => {
+  let tile = document.createElement('div')
+  
+  let titleElement = document.createElement('h1')
+  tile.appendChild(titleElement)
+  titleElement.textContent = film.title
+  
+  let crawlElement = document.createElement('div')
+  tile.appendChild(crawlElement)
+  crawlElement.textContent = film.opening_crawl
 
-    titleElement.textContent = film.title
-    crawlElement.textContent = film.opening_crawl
-
-    intro.appendChild(titleElement)
-    intro.appendChild(crawlElement)
-    intro.appendChild(document.createElement('hr'))
-})
-
+  intro.appendChild(tile);
+ 
+});
 
 // let titleElement = document.querySelector('.title')
 // let crawlElement = document.querySelector('.crawl')
