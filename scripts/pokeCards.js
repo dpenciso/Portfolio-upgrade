0ebbbb477bef47d295346c486f2a560bc0ad212e
David Enciso <dpenciso@msn.com>
2019-03-28 12:54:18 -0600

    imported new images and created card generator

--- scripts/pokeCards.js
+++ scripts/pokeCards.js
@@ -1,29 +1,73 @@
 import { pokemon } from "../data/pokemon.js";
 
-console.log(pokemon);
+class Pokemon {
+  constructor(id) {
+    this.id = id;
+  }
+}
 
 const mainContainer = document.querySelector(".container");
 
 function createPokeCard(pokeData) {
   let card = document.createElement("div");
-  card.className = 'box'
-  let title = document.createElement("h2");
+  card.className = "box";
+  let figure = document.createElement("figure");
+  let caption = document.createElement("figcaption");
   let image = document.createElement("img");
 
-  let upperName = pokeData.name.charAt(0).toUpperCase() + pokeData.name.slice(1)
-  title.textContent = upperName;
-  image.src = pokeData.sprites.front_shiny;
-  card.appendChild(title);
-  card.appendChild(image);
+  // let upperName =
+  //   pokeData.name.charAt(0).toUpperCase() + pokeData.name.slice(1);
+  caption.textContent = pokeData.name
+  if (pokeData.id !== 0) {
+    image.src = `../images/${pokeData.imageID}${pokeData.name}.png`;
+  } else {
+    image.src = '../images/pokeball.png'
+  }
+
+  figure.appendChild(image);
+  figure.appendChild(caption);
+  card.appendChild(figure);
   mainContainer.appendChild(card);
 }
 
-pokemon.forEach(singleMon => {
-  fetch(singleMon.url)
+// pokemon.forEach(singleMon => {
+//   fetch(singleMon.url)
+//     .then(function(response) {
+//       return response.json();
+//     })
+//     .then(function(myJson) {
+//       createPokeCard(myJson);
+//     });
+// });
+
+function fetchSinglePokemon(id) {
+  fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
     .then(function(response) {
-      return response.json();
+      return response.json()
     })
-    .then(function(myJson) {
-      createPokeCard(myJson);
-    });
-});
+    .then(function(retrievedPokemon) {
+      if(retrievedPokemon.id < 10) {
+          retrievedPokemon.imageID = '00' + retrievedPokemon.id
+        }
+        if(retrievedPokemon.id > 9 && retrievedPokemon.id <100) {
+          retrievedPokemon.imageID = '0' + retrievedPokemon.id
+        }
+        if(retrievedPokemon.id > 99) {
+          retrievedPokemon.imageID = retrievedPokemon.id
+        }
+      retrievedPokemon.name = retrievedPokemon.name.charAt(0).toUpperCase() + retrievedPokemon.name.slice(1)
+      createPokeCard(retrievedPokemon)
+})
+}
+const newPokemonButton = document.querySelector('button');
+
+newPokemonButton.addEventListener('click', function() {
+  let pokemonID = prompt("Enter an ID of an existing pokemon:");
+  fetchSinglePokemon(pokemonID)
+//   const retrievedPokemon = fetchSinglePokemon(pokemonID);
+//   console.log(retrievedPokemon)
+//   if(pokemonID.length === 1) {
+//     pokemonID = '00' + pokemonID
+//   }
+//   createPokeCard(retrievedPokemon)
+})
