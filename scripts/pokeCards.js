ee0a41870174b920d5e5dca7c5ec1f61e0df3a7b
David Enciso <dpenciso@msn.com>
2019-04-02 11:48:34 -0600

    initially load page with 25 pokemon fixed

--- scripts/pokeCards.js
+++ scripts/pokeCards.js
@@ -30,15 +30,29 @@ function createPokeCard(pokeData) {
   mainContainer.appendChild(card);
 }
 
-// pokemon.forEach(singleMon => {
-//   fetch(singleMon.url)
-//     .then(function(response) {
-//       return response.json();
-//     })
-//     .then(function(myJson) {
-//       createPokeCard(myJson);
-//     });
-// });
+pokemon.forEach(singleMon => {
+  fetch(singleMon.url)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(myJson) {
+      createPokeCard(matchIdToImage(myJson));
+    });
+});
+
+function matchIdToImage(aPokemon) {
+    if(aPokemon.id < 10) {
+          aPokemon.imageID = '00' + aPokemon.id
+        }
+        if(aPokemon.id > 9 && aPokemon.id <100) {
+          aPokemon.imageID = '0' + aPokemon.id
+        }
+        if(aPokemon.id > 99) {
+          aPokemon.imageID = aPokemon.id
+        }
+      aPokemon.name = aPokemon.name.charAt(0).toUpperCase() + aPokemon.name.slice(1)
+  return aPokemon
+}
 
 function fetchSinglePokemon(id) {
   fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
@@ -46,19 +60,11 @@ function fetchSinglePokemon(id) {
       return response.json()
     })
     .then(function(retrievedPokemon) {
-      if(retrievedPokemon.id < 10) {
-          retrievedPokemon.imageID = '00' + retrievedPokemon.id
-        }
-        if(retrievedPokemon.id > 9 && retrievedPokemon.id <100) {
-          retrievedPokemon.imageID = '0' + retrievedPokemon.id
-        }
-        if(retrievedPokemon.id > 99) {
-          retrievedPokemon.imageID = retrievedPokemon.id
-        }
-      retrievedPokemon.name = retrievedPokemon.name.charAt(0).toUpperCase() + retrievedPokemon.name.slice(1)
-      createPokeCard(retrievedPokemon)
+      
+      createPokeCard(matchIdToImage(retrievedPokemon))
 })
 }
+
 const newPokemonButton = document.querySelector('button');
 
 newPokemonButton.addEventListener('click', function() {
