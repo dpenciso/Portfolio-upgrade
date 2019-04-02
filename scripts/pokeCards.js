355a2c5273f19f193cb14e974c124f15700fad24
David Enciso <dpenciso@msn.com>
2019-04-02 13:10:46 -0600

    added flip animation and color

--- scripts/pokeCards.js
+++ scripts/pokeCards.js
@@ -1,79 +1,97 @@
-import { pokemon } from "../data/pokemon.js";
+import { pokemon } from '../data/pokemon.js'
 
 class Pokemon {
-  constructor(id) {
-    this.id = id;
-  }
+    constructor(id) {
+        this.id = id
+    }
 }
 
-const mainContainer = document.querySelector(".container");
+const mainContainer = document.querySelector('.container')
+
+function cardFront(pokeData) {
+    let cardFront = document.createElement('div')
+    cardFront.className = 'card__face'
+    let figure = document.createElement('figure')
+    let caption = document.createElement('figcaption')
+    let image = document.createElement('img')
+
+    caption.textContent = pokeData.name
+    if(pokeData.id !== 0) {
+        image.src = `../images/${pokeData.imageID}${pokeData.name}.png`
+    } else {
+        image.src = `../images/pokemon_card_backside.png`
+    }
+    
+    figure.appendChild(image)
+    figure.appendChild(caption)
+    cardFront.appendChild(figure)
+    return cardFront
+}
+
+function cardBack(pokeData) {
+    let cardBack = document.createElement('div')
+    let backImage = document.createElement('img')
+    backImage.src = `../images/pokemon_card_backside.png`
+    cardBack.className = 'card__face card__face--back'
+    cardBack.appendChild(backImage)
+    return cardBack
+}
 
 function createPokeCard(pokeData) {
-  let card = document.createElement("div");
-  card.className = "box";
-  let figure = document.createElement("figure");
-  let caption = document.createElement("figcaption");
-  let image = document.createElement("img");
+    let scene = document.createElement('div')
+    scene.className = 'scene'
+    let card = document.createElement('div')
+    card.className = 'card'
 
-  // let upperName =
-  //   pokeData.name.charAt(0).toUpperCase() + pokeData.name.slice(1);
-  caption.textContent = pokeData.name
-  if (pokeData.id !== 0) {
-    image.src = `../images/${pokeData.imageID}${pokeData.name}.png`;
-  } else {
-    image.src = '../images/pokeball.png'
-  }
+    card.appendChild(cardFront(pokeData))
+    card.appendChild(cardBack(pokeData))
 
-  figure.appendChild(image);
-  figure.appendChild(caption);
-  card.appendChild(figure);
-  mainContainer.appendChild(card);
+    card.addEventListener( 'click', function() {
+        card.classList.toggle('is-flipped');
+      })
+
+    scene.appendChild(card)
+    mainContainer.appendChild(scene)
 }
 
 pokemon.forEach(singleMon => {
-  fetch(singleMon.url)
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(myJson) {
-      createPokeCard(matchIdToImage(myJson));
-    });
-});
-
-function matchIdToImage(aPokemon) {
-    if(aPokemon.id < 10) {
-          aPokemon.imageID = '00' + aPokemon.id
-        }
-        if(aPokemon.id > 9 && aPokemon.id <100) {
-          aPokemon.imageID = '0' + aPokemon.id
-        }
-        if(aPokemon.id > 99) {
-          aPokemon.imageID = aPokemon.id
-        }
-      aPokemon.name = aPokemon.name.charAt(0).toUpperCase() + aPokemon.name.slice(1)
-  return aPokemon
-}
-
-function fetchSinglePokemon(id) {
-  fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+    fetch(singleMon.url)
     .then(function(response) {
       return response.json()
     })
-    .then(function(retrievedPokemon) {
-      
-      createPokeCard(matchIdToImage(retrievedPokemon))
+    .then(function(myJson) {
+      createPokeCard(matchIdToImage(myJson))
+    })
 })
+
+function matchIdToImage(aPokemon) {
+    if(aPokemon.id < 10) {
+        aPokemon.imageID = "00" + aPokemon.id
+    }
+    if(aPokemon.id > 9 && aPokemon.id < 100 ) {
+        aPokemon.imageID = "0" + aPokemon.id
+    }
+    if(aPokemon.id > 99) {
+        aPokemon.imageID = aPokemon.id
+    }
+    aPokemon.name = aPokemon.name.charAt(0).toUpperCase() + aPokemon.name.slice(1)
+    return aPokemon
 }
 
-const newPokemonButton = document.querySelector('button');
+function fetchSinglePokemon(id) {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(retrievedPokemon) {
+        createPokeCard(matchIdToImage(retrievedPokemon))
+    })
+}
+
+
+const newPokemonButton = document.querySelector('button')
 
 newPokemonButton.addEventListener('click', function() {
-  let pokemonID = prompt("Enter an ID of an existing pokemon:");
-  fetchSinglePokemon(pokemonID)
-//   const retrievedPokemon = fetchSinglePokemon(pokemonID);
-//   console.log(retrievedPokemon)
-//   if(pokemonID.length === 1) {
-//     pokemonID = '00' + pokemonID
-//   }
-//   createPokeCard(retrievedPokemon)
-})
+    let pokemonID = prompt('Enter an ID of an existing pokemon:')
+    fetchSinglePokemon(pokemonID)
+  });
