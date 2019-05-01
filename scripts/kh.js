e5e808ce79d8dc314f287be8e998f4db143a4bc0
David Enciso <dpenciso@msn.com>
2019-05-01 13:39:30 -0600

    separated into groups

--- scripts/kh.js
+++ scripts/kh.js
@@ -10,83 +10,83 @@ const mainContainer = document.createElement("div");
 
 mainContainer.className = "container";
 
-people.forEach(person => {
-  let characterElement = document.createElement("div");
-  let homeworldElement = document.createElement("p");
-  let imageElement = document.createElement("img");
-
-  characterElement.className = "box";
-  characterElement.textContent = person.name;
-  homeworldElement.textContent = person.homeworld;
-  imageElement.src = person.img_url;
-
-  mainContainer.appendChild(characterElement);
-  characterElement.appendChild(homeworldElement);
-  characterElement.appendChild(imageElement);
-});
-
-document.body.appendChild(mainContainer);
-
-
-//SEPARATED INTO GROUPS
-
-// const destiny = people.filter(person => person.group === "Destiny Trio");
-// const wayfinder = people.filter(person => person.group === "Wayfinder Trio");
-// const seasalt = people.filter(person => person.group === "Seasalt Trio");
-
-// destiny.forEach(destiny => {
+// people.forEach(person => {
 //   let characterElement = document.createElement("div");
 //   let homeworldElement = document.createElement("p");
 //   let imageElement = document.createElement("img");
 
 //   characterElement.className = "box";
-//   characterElement.textContent = destiny.name;
-//   homeworldElement.textContent = destiny.homeworld;
-//   imageElement.src = destiny.img_url;
+//   characterElement.textContent = person.name;
+//   homeworldElement.textContent = person.homeworld;
+//   imageElement.src = person.img_url;
 
 //   mainContainer.appendChild(characterElement);
 //   characterElement.appendChild(homeworldElement);
 //   characterElement.appendChild(imageElement);
 // });
 
-// const secondContainer = document.createElement("div");
-// secondContainer.className = "containerTwo";
+document.body.appendChild(mainContainer);
 
-// seasalt.forEach(seasalt => {
-//   let characterElement = document.createElement("div");
-//   let homeworldElement = document.createElement("p");
-//   let imageElement = document.createElement("img");
 
-//   characterElement.className = "box";
-//   characterElement.textContent = seasalt.name;
-//   homeworldElement.textContent = seasalt.homeworld;
-//   imageElement.src = seasalt.img_url;
+//SEPARATED INTO GROUPS
 
-//   secondContainer.appendChild(characterElement);
-//   characterElement.appendChild(homeworldElement);
-//   characterElement.appendChild(imageElement);
-// });
+const destiny = people.filter(person => person.group === "Destiny Trio");
+const wayfinder = people.filter(person => person.group === "Wayfinder Trio");
+const seasalt = people.filter(person => person.group === "Seasalt Trio");
 
-// document.body.appendChild(secondContainer);
+destiny.forEach(destiny => {
+  let characterElement = document.createElement("div");
+  let homeworldElement = document.createElement("p");
+  let imageElement = document.createElement("img");
 
-// const thirdContainer = document.createElement("div");
-// thirdContainer.className = "containerThree";
+  characterElement.className = "box";
+  characterElement.textContent = destiny.name;
+  homeworldElement.textContent = destiny.homeworld;
+  imageElement.src = destiny.img_url;
 
-// wayfinder.forEach(wayfinder => {
-//   let characterElement = document.createElement("div");
-//   let homeworldElement = document.createElement("p");
-//   let imageElement = document.createElement("img");
+  mainContainer.appendChild(characterElement);
+  characterElement.appendChild(homeworldElement);
+  characterElement.appendChild(imageElement);
+});
 
-//   characterElement.className = "box";
-//   characterElement.textContent = wayfinder.name;
-//   homeworldElement.textContent = wayfinder.homeworld;
-//   imageElement.src = wayfinder.img_url;
+const secondContainer = document.createElement("div");
+secondContainer.className = "containerTwo";
 
-//   thirdContainer.appendChild(characterElement);
-//   characterElement.appendChild(homeworldElement);
-//   characterElement.appendChild(imageElement);
-// });
+seasalt.forEach(seasalt => {
+  let characterElement = document.createElement("div");
+  let homeworldElement = document.createElement("p");
+  let imageElement = document.createElement("img");
 
-// document.body.appendChild(thirdContainer);
+  characterElement.className = "box";
+  characterElement.textContent = seasalt.name;
+  homeworldElement.textContent = seasalt.homeworld;
+  imageElement.src = seasalt.img_url;
 
-// console.log(seasalt);
+  secondContainer.appendChild(characterElement);
+  characterElement.appendChild(homeworldElement);
+  characterElement.appendChild(imageElement);
+});
+
+document.body.appendChild(secondContainer);
+
+const thirdContainer = document.createElement("div");
+thirdContainer.className = "containerThree";
+
+wayfinder.forEach(wayfinder => {
+  let characterElement = document.createElement("div");
+  let homeworldElement = document.createElement("p");
+  let imageElement = document.createElement("img");
+
+  characterElement.className = "box";
+  characterElement.textContent = wayfinder.name;
+  homeworldElement.textContent = wayfinder.homeworld;
+  imageElement.src = wayfinder.img_url;
+
+  thirdContainer.appendChild(characterElement);
+  characterElement.appendChild(homeworldElement);
+  characterElement.appendChild(imageElement);
+});
+
+document.body.appendChild(thirdContainer);
+
+console.log(seasalt);
